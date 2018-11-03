8ff6680c6f0d17b51e36dd822e9a855762e62202
Anawat <anawat@perfenterprise.com>
2018-11-03 16:00:38 +0700

    commit completed code
    Signed-off-by: Anawat <anawat@perfenterprise.com>

---
+++ HackChange/HackChange/HackChangeCode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HackChange
+{
+    public class ChangeOut
+    {
+    }
+    public class HackChangeCode
+    {
+        public new Dictionary<string, string> Change(int amout, int payment)
+        {
+            var data = new Dictionary<string, string>();
+
+            if (payment - amout < 0)
+            {
+                data.Add("Note", "เงินไม่พอ");
+                return data;
+            }
+            var sum = payment - amout;
+            data.Add("Note", string.Empty);
+            data.Add("Change", sum.ToString());
+
+            sum = BillCalculate(sum, 1000, data);
+            sum = BillCalculate(sum, 500, data);
+            sum = BillCalculate(sum, 100, data);
+            sum = BillCalculate(sum, 50, data);
+            sum = BillCalculate(sum, 20, data);
+            sum = BillCalculate(sum, 5, data);
+            sum = BillCalculate(sum, 1, data);
+
+            return data;
+        }
+        
+        private static int BillCalculate(int sum, int billValue, Dictionary<string, string> data)
+        {
+            var value = sum / billValue;
+            data.Add("Bill" + billValue, value.ToString());
+            sum -= billValue * value;
+            return sum;
+        }
+
+    }
+}
---
+++ HackChange/HackChangeTest/HackChangeUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using HackChange;
+using System;
+using Xunit;
+
+namespace HackChangeTest
+{
+    public class HackChangeUnitTesta
+    {
+        //จำนวนเงิน/จำนวนเงินที่จ่ายมา/เงินถอน/1000/500/100/50/20/5/1
+        [Theory]
+        [InlineData(250, 250, 0, 0, 0, 0, 0, 0, 0, 0)]
+        [InlineData(2000, 3000, 1000, 1, 0, 0, 0, 0, 0, 0)]
+        [InlineData(2000, 2500, 500, 0, 1, 0, 0, 0, 0, 0)]
+        [InlineData(900, 2500, 1600, 1, 1, 1, 0, 0, 0, 0)]
+        [InlineData(50, 100, 50, 0, 0, 0, 1, 0, 0, 0)]
+        [InlineData(360, 500, 140, 0, 0, 1, 0, 2, 0, 0)]
+        [InlineData(30, 300, 270, 0, 0, 2, 1, 1, 0, 0)]
+        [InlineData(94, 100, 6, 0, 0, 0, 0, 0, 1, 1)]
+        [InlineData(23, 100, 77, 0, 0, 0, 1, 1, 1, 2)]
+
+        public void ChangeAble(int Amout, int Payment, int Change, int Bill1000, int Bill500, int Bill100, int Bill50, int Bill20, int Bill5, int Bill1)
+        {
+            var HackChange = new HackChangeCode();
+            var result = HackChange.Change(Amout, Payment);
+            result.TryGetValue("Change", out var value);
+            Assert.Equal(Change.ToString(), value);
+            result.TryGetValue("Bill1000", out value);
+            Assert.Equal(Bill1000.ToString(), value);
+            result.TryGetValue("Bill500", out value);
+            Assert.Equal(Bill500.ToString(), value);
+            result.TryGetValue("Bill100", out value);
+            Assert.Equal(Bill100.ToString(), value);
+            result.TryGetValue("Bill50", out value);
+            Assert.Equal(Bill50.ToString(), value);
+            result.TryGetValue("Bill20", out value);
+            Assert.Equal(Bill20.ToString(), value);
+            result.TryGetValue("Bill5", out value);
+            Assert.Equal(Bill5.ToString(), value);
+            result.TryGetValue("Bill1", out value);
+            Assert.Equal(Bill1.ToString(), value);
+        }
+
+        [Theory]
+        [InlineData(1000, 900, "เงินไม่พอ")]
+        public void FailEdPayment(int Amout, int Payment, string expected)
+        {
+            var HackChange = new HackChangeCode();
+            var result = HackChange.Change(Amout, Payment);
+            result.TryGetValue("Note", out var value);
+            Assert.Equal(expected, value);
+        }
+    }
+}
